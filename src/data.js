1fcfb5b58e639f35c7192e7d2463518f47e3ae79
Josk21758 <mwansakunda530@gmail.com>
2026-08-18 23:16:27 +0200

    Updated project tiles

--- src/data.js
+++ src/data.js
@@ -4,7 +4,8 @@ export const projects = [
     title: "E-Council services System",
     description:
       "A full-stack e-council portal system, previously designed as a school project. The Platforms allow form application submissions and Administrative functionalities like Reviewing, Approving and Rejecting citizen based applications. ",
-    image: "../images/permit.jpg",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRubJ5fOSXzaB-Hi-TLOFt8hZ4POeqqUC7KlF2CQqhpy_18l-50Td-_1WJd&s=10",
     tags: ["Next Js", "MongoDB", "Nodemailer"],
     gitLink: "https://github.com/Joshk21758/Permit-Tracking-System",
     link: "https://permit-tracking-system-6g3d.vercel.app/",
@@ -14,7 +15,8 @@ export const projects = [
     title: "Medical Care Booking system",
     description:
       "An Online booking system for Lusaka's New Life Medical Centre. Currently under development.",
-    image: "../images/care.jpg",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdgDDMbHHU-mzk_sp2U2dgDIDV_5NDZtyLpjnmgUAG0ZF1BweVohkeNr7M&s=10",
     tags: ["React", "MongoDB", "Nodemailer", "Tailwind CSS"],
     gitLink: "https://github.com/Joshk21758/Med-care",
     link: "https://exquisite-bavarois-8145d4.netlify.app/",
