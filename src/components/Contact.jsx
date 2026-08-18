155331ddf0fd15194788d2b403be2972d3316454
Josk21758 <mwansakunda530@gmail.com>
2026-08-18 22:00:56 +0200

    Updated contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -134,7 +134,7 @@ export default function Contact() {
               className="w-full md:w-auto px-8 py-4 bg-gray-700 font-custom text-lg text-white rounded-3xl font-medium hover:bg-blue-700 focus:ring-4 focus:ring-blue-600/20 transition-all flex items-center justify-center gap-2 disabled:opacity-70 disabled:cursor-not-allowed cursor-pointer"
             >
               {isPending ?
-                <Loader2 className="w-5 h-5 ml-15 animate-spin" color="white" />
+                <Loader2 className="w-5 h-5 ml-10 animate-spin" color="white" />
               : "Send Message"}
             </button>
           </form>
